8fc0c7c3a037173b5fcf80523887e9acab99f560
Kritika Srivastava <kritikasri27@gmail.com>
2023-02-28 02:50:26 +0530

    Created homepage

---
+++ homepage/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+
+export const Home = () => {
+  return (
+    <section>
+        <div className='hero'></div>
+        <div className='ad'>
+            <div className='container'>
+<h1>Ready for New Stuff</h1>
+<button>Get Started</button>
+            </div>
+        </div>
+    </section>
+  )
+}
---
+++ homepage/src/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./App.css";
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <h1>Trends</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Products</li>
+        </ul>
+        <ul>
+            <li>Cart</li>
+            <li>Fav</li>
+        </ul>
+    </nav>
+  )
+}
