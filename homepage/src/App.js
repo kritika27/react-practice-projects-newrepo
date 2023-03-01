f68a4f1e5acc91a4e2d6e2e34a1e189b6ee8573f
Kritika Srivastava <kritikasri27@gmail.com>
2023-03-01 23:54:31 +0530

    Added Footer

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Navbar } from './Navbar';
 import { Home } from './Home';
-
+import { Footer } from './Footer';
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
