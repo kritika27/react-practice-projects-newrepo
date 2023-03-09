b08faddbe91cb9232f16b9d644796b2989f9423c
Kritika Srivastava <kritikasri27@gmail.com>
2023-03-10 00:08:03 +0530

    Added services

--- homepage/src/Services.js
+++ homepage/src/Services.js
@@ -3,10 +3,12 @@ import "./App.css";
 
 export const Services = () => {
   return (
-    <section>
-        <div>
-            hi
+    <div className='services'>
+        <div className='services-item'>
+            <div className='item'><img src="https://i.ibb.co/7CQVJNm/blue-tank.png" alt="service"/></div>
+            <div className='item'><img src="https://i.ibb.co/7CQVJNm/blue-tank.png" alt="service"/></div>
+            <div className='item'><img src="https://i.ibb.co/7CQVJNm/blue-tank.png" alt="service"/></div>
         </div>
-    </section>
+    </div>
   )
 }
